8222d3b12b0eabd0f0c14cae3bc534c960224f06
Varun Kohli <kohli.varun5@gmail.com>
2018-08-05 18:47:11 -0400

    .

--- sol/Event.sol
+++ sol/Event.sol
@@ -3,14 +3,20 @@ pragma solidity ^0.4.24;
 import "./ERC721.sol";
 
 contract Event is ERC721 {
+    
+  struct TicketInfo {
+    uint256 d_orig_price;
+  }
  
-  address internal d_creator;
+  uint256 internal d_creator_commission_factor;
+ 
+  address internal d_admin;
   address internal d_organizer;
   
   string internal d_description;
   
   // Array with all token ids, used for enumeration
-  uint256[] internal d_ticket_ids;
+  TicketInfo[] internal d_tickets;
   
   // Mapping from owner to list of owned token IDs
   mapping(address => uint256[]) internal d_owner_tokens;
@@ -18,16 +24,71 @@ contract Event is ERC721 {
   // Mapping from token id to owner address
   mapping(uint256 => address) internal d_token_owner;
   
-  // Mapping from token ID to approved address
-  mapping (uint256 => address) internal d_token_a;
-  
 
-  constructor(string _description, uint256 _numTickets) public {
+  constructor(string _description, address _organizer) public { 
+    d_admin = msg.sender;
+    d_organizer = _organizer;
     d_description = _description;
-    for (uint256 i=0;i<_numTickets;++i) {
-      d_ticket_ids.push(i);
+  }
+  
+  function issue(uint256 _numTickets,uint256 _price) public {
+    for(uint256 i=0;i<_numTickets;++i) {
+      d_tickets.push(TicketInfo({d_orig_price:_price}));
     }
-  }   
+  }
+  
+  function buy(uint256 _numTickets) public payable {
+    uint256 total_cost=0;
+    uint256 bought=0;
+    
+    // We will buy 1 ticket at a time
+    // If while buying, we do not find enough tickets, 
+    // or we did not get enough money, we throw
+    for(uint256 i=0;i<d_tickets.length && i < _numTickets;++i) {
+      if (d_token_owner[i] != address(0)) { continue; }
+        
+      // Ticket can be bought 
+      total_cost+=d_tickets[i].d_orig_price;
+      d_owner_tokens[msg.sender].push(i);
+      d_token_owner[i] = msg.sender;
+      bought++;
+  
+    }
+    
+    require(bought == _numTickets);
+    require(total_cost <= msg.value);
+    
+    // Take admin cut
+    address(d_admin).transfer(msg.value / d_creator_commission_factor);
+  }
+  
+  function getBalance() public view returns(uint) {
+    require(msg.sender == d_admin || msg.sender == d_organizer);
+    return address(this).balance;
+  }
+  
+  function withdraw() public {
+    require(msg.sender == d_organizer);
+    address(d_organizer).transfer(getBalance());
+  }
+  
+  function numSold() public view returns(uint256) {
+    require(msg.sender == d_admin || msg.sender == d_organizer);
+    uint256 numSoldCount=0;
+    for(uint256 i=0;i<d_tickets.length;++i) {
+      if (d_token_owner[i] != address(0)) { numSoldCount++;}
+    }
+    return numSoldCount;
+  }
+  
+  function numUnSold() public view returns(uint256) {
+    require(msg.sender == d_admin || msg.sender == d_organizer);
+    uint256 numUnSoldCount=0;
+    for(uint256 i=0;i<d_tickets.length;++i) {
+      if (d_token_owner[i] == address(0)) { numUnSoldCount++; }
+    }
+    return numUnSoldCount;
+  }
   
   function description() public view returns(string) {
     return d_description;
@@ -42,7 +103,7 @@ contract Event is ERC721 {
   }
   
   function exists(uint256 _tokenId) public view returns (bool _exists) {
-    return _tokenId >= 0 && _tokenId < d_ticket_ids.length;
+    return _tokenId >= 0 && _tokenId < d_tickets.length;
   }
 
   function approve(address _to, uint256 _tokenId) public;
@@ -54,16 +115,14 @@ contract Event is ERC721 {
     public view returns (bool);
 
   function transferFrom(address _from, address _to, uint256 _tokenId) public;
-  function safeTransferFrom(address _from, address _to, uint256 _tokenId)
-    public;
+  function safeTransferFrom(address _from, address _to, uint256 _tokenId) public;
 
   function safeTransferFrom(
     address _from,
     address _to,
     uint256 _tokenId,
     bytes _data
-  )
-    public;
+  ) public;
 
   
   
--- sol/Universe.sol
+++ sol/Universe.sol
@@ -1,25 +1,24 @@
 pragma solidity ^0.4.24;
 
 contract Universe {
+  
+  address d_owner;
+  
+  constructor() public {
+    d_owner = msg.sender;
+  }
+  
+  function createEvent() public payable {
     
-    address d_owner;
-    
-    constructor() public {
-        d_owner = msg.sender;
-    }
-    
-    function createEvent() public payable {
-        
-    }
-    
-    function getBalance() public constant returns(uint) {
-        return address(this).balance;
-    }
-    
-    function withdraw() public {
-        require(msg.sender == d_owner);
-        address(d_owner).transfer(getBalance());
-    }
-    
-    
+  }
+  
+  function getBalance() public constant returns(uint) {
+    return address(this).balance;
+  }
+  
+  function withdraw() public {
+    require(msg.sender == d_owner);
+    address(d_owner).transfer(getBalance());
+  }
+  
 }
