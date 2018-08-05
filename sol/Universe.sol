13cb7f530f1ce19f9284f4e9f962b1069628b8f3
Varun Kohli <kohli.varun5@gmail.com>
2018-08-05 15:45:05 -0400

    .

--- sol/Universe.sol
+++ sol/Universe.sol
@@ -1,3 +1,25 @@
+pragma solidity ^0.4.24;
+
 contract Universe {
-    mapping(address => uint256) map;
+    
+    address d_owner;
+    
+    constructor() public {
+        d_owner = msg.sender;
+    }
+    
+    function createEvent() public payable {
+        
+    }
+    
+    function getBalance() public constant returns(uint) {
+        return address(this).balance;
+    }
+    
+    function withdraw() public {
+        require(msg.sender == d_owner);
+        address(d_owner).transfer(getBalance());
+    }
+    
+    
 }
