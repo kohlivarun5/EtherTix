6ed9cbc9be603f5efe5e1216bf53c77ab82e479e
Varun Kohli <kohli.varun5@gmail.com>
2021-02-28 13:45:55 -0500

    .

--- sol/Event.sol
+++ sol/Event.sol
@@ -11,111 +11,99 @@ contract Event is IERC721 {
   string public description;
   string public imgSrc; // Can be data or link
   string public externalLink;
- 
-  uint256 internal d_creator_commission_percent = 1;
- 
-  address payable internal d_admin;
-  address payable public d_organizer;
-  
-  // Array with all token ids, used for enumeration
-  EventImpl.TicketInfo[] internal d_tickets;
-  
-  // Mapping from owner to list of owned token IDs
-  mapping(address => uint256[]) internal d_owner_tokens;
-  
-  // Mapping from token id to owner address
-  mapping(uint256 => address payable) internal d_token_owner;
-  
-  // For transfers 
-  mapping(uint256 => uint256) internal d_token_ask;
-  uint256 d_token_ask_num;
-
   bool public d_mark_delete;
 
+  using EventImpl for EventData;
+  EventData internal d_data;
+
   constructor(string memory _description, address payable _organizer,uint256 commission_percent) public { 
+    TicketInfo[] memory tickets;
     description = _description;
-    d_admin = msg.sender;
-    d_organizer = _organizer;
-    d_token_ask_num=0;
-    d_creator_commission_percent=commission_percent;
+    d_data = EventData({
+      d_admin:msg.sender,
+      d_organizer:_organizer,
+      d_token_ask_num:0,
+      d_creator_commission_percent:commission_percent,
+      d_tickets:tickets
+    });
     d_mark_delete=false;
   }
   
   function setImg(string memory _imgSrc) public {
-      require(msg.sender == d_organizer);
+      require(msg.sender == d_data.d_organizer);
       imgSrc = _imgSrc;
   }
   
   function setExternalLink(string memory _externalLink) public {
-      require(msg.sender == d_organizer);
+      require(msg.sender == d_data.d_organizer);
       externalLink = _externalLink;
   }
   
   function issue(uint256 _numTickets,uint256 _price) public {
-    require(msg.sender == d_organizer);
+    require(msg.sender == d_data.d_organizer);
     for(uint256 i=0;i<_numTickets;++i) {
-      d_tickets.push(EventImpl.TicketInfo({d_prev_price:_price,d_used:false}));
+      d_data.d_tickets.push(TicketInfo({d_prev_price:_price,d_used:false}));
     }
   }
 
   function getCostFor(uint256 _numTickets) public view returns(uint256) {
-    return EventImpl.getCostFor(_numTickets, d_tickets, d_token_owner);
+    return d_data.getCostFor(_numTickets);
   }
   
   function buy(uint256 _numTickets) public payable {
-    EventImpl.buy(_numTickets,d_tickets,d_token_owner,d_owner_tokens,d_admin,d_creator_commission_percent);
+    return d_data.buy(_numTickets,msg.sender);
   }
   
   function getBalance() public view returns(uint) {
-    require(msg.sender == d_admin || msg.sender == d_organizer);
+    require(msg.sender == d_data.d_admin || msg.sender == d_data.d_organizer);
     return address(this).balance;
   }
   
   function withdraw() public {
-    require(msg.sender == d_organizer);
-    d_organizer.transfer(getBalance());
+    require(msg.sender == d_data.d_organizer);
+    return d_data.d_organizer.transfer(getBalance());
   }
   
   function numSold() public view returns(uint256) {
-    return EventImpl.numSold(d_tickets,d_token_owner);
+    return d_data.numSold();
   }
   
   function numUnSold() public view returns(uint256) {
-    return EventImpl.numUnSold(d_tickets,d_token_owner);
+    return d_data.numUnSold();
   }
 
   function numUsed() public view returns(uint256) {
-    return EventImpl.numUsed(d_tickets);
+    return d_data.numUsed();
   }
 
   function numToBeUsed() public view returns(uint256) {
-    return EventImpl.numToBeUsed(d_tickets,d_token_owner);
+    return d_data.numToBeUsed();
   }
   
   function balanceOf(address _owner) public view override returns (uint256 _balance) {
-    return d_owner_tokens[_owner].length;    
+    return d_data.d_owner_tokens[_owner].length;    
   }
   
   function ownerOf(uint256 _tokenId) public view override returns (address _owner) {
-    return d_token_owner[_tokenId];    
+    return d_data.d_token_owner[_tokenId];    
   }
   
   function exists(uint256 _tokenId) public view returns (bool _exists) {
-    return _tokenId >= 0 && _tokenId < d_tickets.length;
+    return _tokenId >= 0 && _tokenId < d_data.d_tickets.length;
   }
   
   function myTickets() public view returns(uint256[] memory,uint256[] memory,bool[] memory,bool[] memory) {
-    return EventImpl.myTickets(d_owner_tokens,d_token_ask,d_tickets);
+    return d_data.myTickets(msg.sender);
   }
   
   function proposeSale(uint256 _token,uint256 _price) public {
-    require(d_tickets[_token].d_used == false, "Ticket already used!");
-    require(d_token_owner[_token] == msg.sender);
+    require(d_data.d_tickets[_token].d_used == false, "Ticket already used!");
+    require(d_data.d_token_owner[_token] == msg.sender);
     require(_price > 0, "Please set a valid non-zero price");
-    if (d_token_ask[_token] == 0) {
-        d_token_ask_num++;
+    if (d_data.d_token_ask[_token] == 0) {
+        d_data.d_token_ask_num++;
     }
-    d_token_ask[_token] = _price;
+    d_data.d_token_ask[_token] = _price;
   }
   
   function proposeSales(uint256[] memory _tokens,uint256[] memory _prices) public {
@@ -126,10 +114,10 @@ contract Event is IERC721 {
   }
   
   function retractSale(uint256 _token) public {
-    require(d_token_owner[_token] == msg.sender);
-    require(d_token_ask[_token] != 0);
-    delete d_token_ask[_token];
-    d_token_ask_num--;
+    require(d_data.d_token_owner[_token] == msg.sender);
+    require(d_data.d_token_ask[_token] != 0);
+    delete d_data.d_token_ask[_token];
+    d_data.d_token_ask_num--;
   }
   
   function retractSales(uint256[] memory _tokens) public {
@@ -139,56 +127,56 @@ contract Event is IERC721 {
   }
 
   function forSale() public view returns(uint256,uint256[] memory,uint256[] memory) {
-    return EventImpl.forSale(d_tickets, d_token_ask, d_token_ask_num);
+    return d_data.forSale();
   }
   
   function hitAsk(uint256 _token) public payable {
-    require(!d_tickets[_token].d_used, "Ticket already used!");
-    require(d_token_ask[_token] > 0 && msg.value >= d_token_ask[_token]);
+    require(!d_data.d_tickets[_token].d_used, "Ticket already used!");
+    require(d_data.d_token_ask[_token] > 0 && msg.value >= d_data.d_token_ask[_token]);
       
     // Value provided, okay to transfer
-    address payable prev_owner = d_token_owner[_token];
+    address payable prev_owner = d_data.d_token_owner[_token];
     transferFromImpl(prev_owner,msg.sender,_token);
     
     // Take money
-    if (d_tickets[_token].d_prev_price > msg.value) {
+    if (d_data.d_tickets[_token].d_prev_price > msg.value) {
       // Selling for less, all money to seller 
       prev_owner.transfer(msg.value);
     } else {
-      uint256 premium = msg.value - d_tickets[_token].d_prev_price;
+      uint256 premium = msg.value - d_data.d_tickets[_token].d_prev_price;
       uint256 seller_premium = premium / 2;
       
       // TODO Review
-      prev_owner.transfer(seller_premium + d_tickets[_token].d_prev_price);
+      prev_owner.transfer(seller_premium + d_data.d_tickets[_token].d_prev_price);
       
       // Other half premium is for the event, and commission out of it 
-      uint256 commission = (seller_premium / 100) * d_creator_commission_percent;
-      d_admin.transfer(commission);
+      uint256 commission = (seller_premium / 100) * d_data.d_creator_commission_percent;
+      d_data.d_admin.transfer(commission);
       
-      d_tickets[_token].d_prev_price = msg.value;
+      d_data.d_tickets[_token].d_prev_price = msg.value;
     }
   }
   
   function ticketUsed(uint256 _tokenId) public view returns(bool) {
-      return d_tickets[_tokenId].d_used;
+      return d_data.d_tickets[_tokenId].d_used;
   }
 
   // https://medium.com/@libertylocked/ec-signatures-and-recovery-in-ethereum-smart-contracts-560b6dd8876
   function ticketVerificationCode(uint256 _tokenId) public view returns(bytes32) {
-    return EventImpl.ticketVerificationCode(_tokenId, d_tickets);
+    return d_data.ticketVerificationCode(_tokenId);
   }
   
   function isOwnerSig(uint256 _tokenId, bytes memory signature) public view returns(bool) {
-    return EventImpl.isOwnerSig(_tokenId, signature, d_tickets,d_token_owner);
+    return d_data.isOwnerSig(_tokenId, signature);
   }
 
   function useTicket(uint256 _tokenId) public returns(bool) {
-    require(msg.sender == d_organizer);
+    require(msg.sender == d_data.d_organizer);
     require(!ticketUsed(_tokenId), "Ticket already used!");
-    d_tickets[_tokenId].d_used = true;
-    if (d_token_ask[_tokenId] != 0) {
-        delete d_token_ask[_tokenId]; // Just in case
-        d_token_ask_num--;
+    d_data.d_tickets[_tokenId].d_used = true;
+    if (d_data.d_token_ask[_tokenId] != 0) {
+        delete d_data.d_token_ask[_tokenId]; // Just in case
+        d_data.d_token_ask_num--;
     }
     return true;
   }
@@ -209,17 +197,17 @@ contract Event is IERC721 {
 */
 
   function transferFromImpl(address _from, address payable _to, uint256 _token) private {
-      require(d_token_owner[_token] == _from);
+      require(d_data.d_token_owner[_token] == _from);
       
       // Value provided, okay to transfer
-      if (d_token_ask[_token] != 0) {
-        delete d_token_ask[_token]; // No more ask 
-        d_token_ask_num--;
+      if (d_data.d_token_ask[_token] != 0) {
+        delete d_data.d_token_ask[_token]; // No more ask 
+        d_data.d_token_ask_num--;
       }
         
-      address prev_owner = d_token_owner[_token];
+      address prev_owner = d_data.d_token_owner[_token];
         
-      uint256[] storage prev_owner_tokens = d_owner_tokens[prev_owner];
+      uint256[] storage prev_owner_tokens = d_data.d_owner_tokens[prev_owner];
       for (uint256 i = 0;i<prev_owner_tokens.length; ++i) {
         if (prev_owner_tokens[i] == _token) {
             uint256 lenBefore = prev_owner_tokens.length;
@@ -229,16 +217,16 @@ contract Event is IERC721 {
         }
       }
         
-      d_token_owner[_token] = _to;
-      d_owner_tokens[_to].push(_token);
+      d_data.d_token_owner[_token] = _to;
+      d_data.d_owner_tokens[_to].push(_token);
       emit Transfer(_from,_to,_token);
         
-      Universe u = Universe(d_admin);
+      Universe u = Universe(d_data.d_admin);
       u.addUserEvent(address(this),_to);
   }
 
   function transferFrom(address _from, address payable _to, uint256 _token) public override {
-      require(d_token_owner[_token] == _from);
+      require(d_data.d_token_owner[_token] == _from);
       require(msg.sender == _from || tx.origin == _from);
       transferFromImpl(_from,_to,_token);
   }
@@ -247,7 +235,7 @@ contract Event is IERC721 {
   { return transferFrom(_from,_to,_tokenId);}
 
   function markDelete(bool mark_delete) public {
-    require(msg.sender == d_organizer);
+    require(msg.sender == d_data.d_organizer);
     d_mark_delete=mark_delete;
   }
 
@@ -262,7 +250,7 @@ contract Event is IERC721 {
   { require(false,"Unsupported"); }
 
 
-  function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) public payable override
+  function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata _data) public payable override
   { require(false,"Unsupported"); }
 
   function supportsInterface(bytes4 interfaceId) external view override returns (bool)
--- sol/EventImpl.sol
+++ sol/EventImpl.sol
@@ -3,25 +3,45 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "./Universe.sol";
 
+struct TicketInfo {
+  uint256 d_prev_price;
+  bool    d_used;
+}
+
+struct EventData {
+
+    address payable d_admin;
+    address payable d_organizer;
+    
+    // Array with all token ids, used for enumeration
+    TicketInfo[] d_tickets;
+    
+    // Mapping from owner to list of owned token IDs
+    mapping(address => uint256[]) d_owner_tokens;
+    
+    // Mapping from token id to owner address
+    mapping(uint256 => address payable) d_token_owner;
+    
+    // For transfers 
+    mapping(uint256 => uint256) d_token_ask;
+    uint256 d_token_ask_num;
+    uint256 d_creator_commission_percent;
+}
+
 library EventImpl {
 
-    struct TicketInfo {
-      uint256 d_prev_price;
-      bool    d_used;
-    }
-
-    function getCostFor(uint256 _numTickets,TicketInfo[] storage d_tickets,mapping(uint256 => address payable) storage d_token_owner) public view returns(uint256) {
+    function getCostFor(EventData storage self,uint256 _numTickets) public view returns(uint256) {
       uint256 total_cost=0;
       uint256 bought=0;
 
       // We will buy 1 ticket at a time
       // If while buying, we do not find enough tickets, 
       // or we did not get enough money, we throw
-      for(uint256 i=0;i<d_tickets.length && bought < _numTickets;++i) {
-        if (d_token_owner[i] != address(0)) { continue; }
+      for(uint256 i=0;i<self.d_tickets.length && bought < _numTickets;++i) {
+        if (self.d_token_owner[i] != address(0)) { continue; }
 
         // Ticket can be bought 
-        total_cost+=d_tickets[i].d_prev_price;
+        total_cost+=self.d_tickets[i].d_prev_price;
         bought++;
       }
 
@@ -30,27 +50,20 @@ library EventImpl {
       return total_cost;
     }
 
-    function buy(uint256 _numTickets,
-                TicketInfo[] storage d_tickets,
-                mapping(uint256 => address payable) storage d_token_owner,
-                mapping(address => uint256[]) storage d_owner_tokens,
-                address payable d_admin,
-                uint256 d_creator_commission_percent
-                )
-                public {
+    function buy(EventData storage self,uint256 _numTickets,address payable to) public {
       uint256 total_cost=0;
       uint256 bought=0;
 
       // We will buy 1 ticket at a time
       // If while buying, we do not find enough tickets, 
       // or we did not get enough money, we throw
-      for(uint256 i=0;i<d_tickets.length && bought < _numTickets;++i) {
-        if (d_token_owner[i] != address(0)) { continue; }
+      for(uint256 i=0;i<self.d_tickets.length && bought < _numTickets;++i) {
+        if (self.d_token_owner[i] != address(0)) { continue; }
 
         // Ticket can be bought 
-        total_cost+=d_tickets[i].d_prev_price;
-        d_owner_tokens[msg.sender].push(i);
-        d_token_owner[i] = msg.sender;
+        total_cost+=self.d_tickets[i].d_prev_price;
+        self.d_owner_tokens[to].push(i);
+        self.d_token_owner[i] = to;
         bought++;
   
       }
@@ -59,99 +72,87 @@ library EventImpl {
       require(total_cost <= msg.value, "Cost is more than transaction value.");
 
       // Take admin cut
-      uint256 commission = (msg.value / 100) * d_creator_commission_percent;
-      d_admin.transfer(commission);
+      uint256 commission = (msg.value / 100) * self.d_creator_commission_percent;
+      self.d_admin.transfer(commission);
 
-      Universe u = Universe(d_admin);
-      u.addUserEvent(address(this),msg.sender);
+      Universe u = Universe(self.d_admin);
+      u.addUserEvent(address(this),to);
     }
 
-    function numSold(
-                TicketInfo[] storage d_tickets,
-                mapping(uint256 => address payable) storage d_token_owner)
-                public view returns(uint256) {
+    function numSold(EventData storage self) public view returns(uint256) {
       uint256 numSoldCount=0;
-      for(uint256 i=0;i<d_tickets.length;++i) {
-        if (d_token_owner[i] != address(0)) { numSoldCount++;}
+      for(uint256 i=0;i<self.d_tickets.length;++i) {
+        if (self.d_token_owner[i] != address(0)) { numSoldCount++;}
       }
       return numSoldCount;
     }
 
-    function numUnSold(
-                TicketInfo[] storage d_tickets,
-                mapping(uint256 => address payable) storage d_token_owner)
-                public view returns(uint256) {
+    function numUnSold(EventData storage self) public view returns(uint256) {
       uint256 numUnSoldCount=0;
-      for(uint256 i=0;i<d_tickets.length;++i) {
-        if (d_token_owner[i] == address(0)) { numUnSoldCount++; }
+      for(uint256 i=0;i<self.d_tickets.length;++i) {
+        if (self.d_token_owner[i] == address(0)) { numUnSoldCount++; }
       }
       return numUnSoldCount;
     }
 
-    function numUsed(TicketInfo[] storage d_tickets) public view returns(uint256) {
+    function numUsed(EventData storage self) public view returns(uint256) {
       uint256 numUsedCount=0;
-      for(uint256 i=0;i<d_tickets.length;++i) {
-        if (d_tickets[i].d_used) { numUsedCount++; } 
+      for(uint256 i=0;i<self.d_tickets.length;++i) {
+        if (self.d_tickets[i].d_used) { numUsedCount++; } 
       }
       return numUsedCount;
     }
 
-    function numToBeUsed(TicketInfo[] storage d_tickets,
-                mapping(uint256 => address payable) storage d_token_owner)
-                public view returns(uint256) {
+    function numToBeUsed(EventData storage self) public view returns(uint256) {
       uint256 numToBeUsedCount=0;
-      for(uint256 i=0;i<d_tickets.length;++i) {
-        if (!d_tickets[i].d_used && d_token_owner[i] != address(0)) { numToBeUsedCount++; } 
+      for(uint256 i=0;i<self.d_tickets.length;++i) {
+        if (!self.d_tickets[i].d_used && self.d_token_owner[i] != address(0)) { numToBeUsedCount++; } 
       }
       return numToBeUsedCount;
     }
 
-    function myTickets(
-        mapping(address => uint256[]) storage d_owner_tokens,
-        mapping(uint256 => uint256) storage d_token_ask,
-        EventImpl.TicketInfo[] storage d_tickets
-    ) public view returns(uint256[] memory,uint256[] memory,bool[] memory,bool[] memory) {
-        uint256[] storage tokens = d_owner_tokens[msg.sender];
+    function myTickets(EventData storage self, address owner) public view returns(uint256[] memory,uint256[] memory,bool[] memory,bool[] memory) {
+        uint256[] storage tokens = self.d_owner_tokens[owner];
         uint256[] memory prices = new uint256[](tokens.length);
         bool[] memory for_sale = new bool[](tokens.length);
         bool[] memory used = new bool[](tokens.length);
         for(uint256 i=0;i<tokens.length;++i)
         {
             uint256 token=tokens[i];
-            if (d_token_ask[token] != 0) {
-                prices[i] = d_token_ask[token];
+            if (self.d_token_ask[token] != 0) {
+                prices[i] = self.d_token_ask[token];
                 for_sale[i] = true;
             } else {
-                prices[i] = d_tickets[token].d_prev_price;
+                prices[i] = self.d_tickets[token].d_prev_price;
                 for_sale[i] = false;
             }
-            used[i] = d_tickets[token].d_used;
+            used[i] = self.d_tickets[token].d_used;
         }
         return (tokens,prices,for_sale,used);
     }
 
-    function forSale(EventImpl.TicketInfo[] storage d_tickets,mapping(uint256 => uint256) storage d_token_ask,uint256 d_token_ask_num) public view returns(uint256,uint256[] memory,uint256[] memory) {
-        uint256[] memory tokens = new uint256[](d_token_ask_num);
-        uint256[] memory asks = new uint256[](d_token_ask_num);
+    function forSale(EventData storage self) public view returns(uint256,uint256[] memory,uint256[] memory) {
+        uint256[] memory tokens = new uint256[](self.d_token_ask_num);
+        uint256[] memory asks = new uint256[](self.d_token_ask_num);
         uint256 iter=0;
-        for(uint256 i=0;i<d_tickets.length;++i)
+        for(uint256 i=0;i<self.d_tickets.length;++i)
         {
-            if (d_token_ask[i] > 0) { 
+            if (self.d_token_ask[i] > 0) { 
                 tokens[iter] = i;
-                asks[iter] = d_token_ask[i];
+                asks[iter] = self.d_token_ask[i];
                 iter++;
             }
         }
-        require(iter == d_token_ask_num);
+        require(iter == self.d_token_ask_num);
         return (iter,tokens,asks);
     }
 
-    function ticketUsed(uint256 _tokenId,EventImpl.TicketInfo[] storage d_tickets) public view returns(bool) {
-        return d_tickets[_tokenId].d_used;
+    function ticketUsed(EventData storage self, uint256 _tokenId) public view returns(bool) {
+        return self.d_tickets[_tokenId].d_used;
     }
 
-    function ticketVerificationCode(uint256 _tokenId,EventImpl.TicketInfo[] storage d_tickets) public view returns(bytes32) {
-        require(!ticketUsed(_tokenId,d_tickets), "Ticket already used!");
+    function ticketVerificationCode(EventData storage self,uint256 _tokenId) public view returns(bytes32) {
+        require(!ticketUsed(self,_tokenId), "Ticket already used!");
         return keccak256(abi.encodePacked(_tokenId,address(this)));
     }
 
@@ -175,10 +176,10 @@ library EventImpl {
     }
 
     
-    function isOwnerSig(uint256 _tokenId,bytes memory signature,EventImpl.TicketInfo[] storage d_tickets,mapping(uint256 => address payable) storage d_token_owner) public view returns(bool) {
+    function isOwnerSig(EventData storage self,uint256 _tokenId,bytes memory signature) public view returns(bool) {
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-        bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, ticketVerificationCode(_tokenId,d_tickets)));
-        return d_token_owner[_tokenId] == 
+        bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, ticketVerificationCode(self,_tokenId)));
+        return self.d_token_owner[_tokenId] == 
                 recover(prefixedHash,signature);
     }
 
